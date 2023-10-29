354d61dcfc26d254fd24350cf1062868b850cfeb
Alex <hello@missingprogram.me>
2023-10-29 22:32:52 +1300

    refactor: changed the BREAK feature to be inclusive of all metatags. Refactor: Added new option reflective of the new addition

--- scripts/tag_separator.py
+++ scripts/tag_separator.py
@@ -1,13 +1,10 @@
-from enum import Enum
 import logging
 import re
+from enum import Enum
 
 import gradio as gr
 from modules import scripts
-from modules.processing import (
-    StableDiffusionProcessing,
-    StableDiffusionProcessingTxt2Img,
-)
+from modules.processing import StableDiffusionProcessing, StableDiffusionProcessingTxt2Img
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -17,6 +14,7 @@ elem_pfx = "fkcommas"
 
 re_spaces = re.compile(r" {2,}", re.I + re.M)
 re_whitespace = re.compile(r"[\t\n\r\f\v]+", re.I + re.M)
+re_all_caps = re.compile(r"(\b[\.\-_\']*[A-Z]+[\.\-_\']*[A-Z]*[\.\-_\']*\b)")
 
 
 class SepCharacter(str, Enum):
@@ -56,6 +54,12 @@ class TagSeparator(scripts.Script):
                     description="Process negative prompt as well",
                     elem_id=f"{elem_pfx}_neg_enabled",
                 )
+                ignoreCaps = gr.Checkbox(
+                    label="Negative",
+                    value=True,
+                    description="Ignores tags in all caps, used for special keywords such as BREAK and AND",
+                    elem_id=f"{elem_pfx}_ignoreCaps",
+                )
                 tag_sep = gr.Dropdown(
                     choices=[x.name for x in SepCharacter],
                     label=extn_name,
@@ -69,10 +73,16 @@ class TagSeparator(scripts.Script):
                     elem_id=f"{elem_pfx}_word_sep",
                 )
 
-        return [enabled, tag_sep, word_sep, neg_enabled]
+        return [enabled, tag_sep, word_sep, neg_enabled, ignoreCaps]
 
     def process(
-        self, p: StableDiffusionProcessing, enabled: bool, tag_sep: str, word_sep: str, neg_enabled: bool
+        self,
+        p: StableDiffusionProcessing,
+        enabled: bool,
+        tag_sep: str,
+        word_sep: str,
+        neg_enabled: bool,
+        ignoreCaps: bool,
     ):
         if enabled is not True:
             return
@@ -88,20 +98,8 @@ class TagSeparator(scripts.Script):
             # build tag list
             prompt_tags = []
 
-            # check for BREAK meta-tag
-            if "BREAK" in prompt:
-                # split prompt into blocks separated by BREAKs
-                prompt_blocks = [x.strip() for x in prompt.split("BREAK")]
-                for block in prompt_blocks:
-                    # split each block into tags separated by commas and add to the list
-                    prompt_tags.extend([x.strip() for x in block.split(",")])
-                    # add a BREAK tag after each block
-                    prompt_tags.append("BREAK")
-                # pop the last BREAK tag since we don't need it
-                prompt_tags.pop()
-            else:
-                # only have one block, split it into tags separated by commas
-                prompt_tags.extend([x.strip() for x in prompt.split(",")])
+            if ignoreCaps:
+                prompt_tags = [x.strip() for x in re.sub(re_all_caps, r",\1,", prompt).split(",")]
 
             # replace spaces with the word separator in each tag
             prompt_tags = [x.replace(" ", word_sep_char) for x in prompt_tags]
